5b9b6a42d6e42a29d79bef5ea9febb35da3c3c48
anjanasrinivas <anjana.srinivas@berkeley.edu>
2016-01-24 11:42:21 -0800

    problem 3

--- problem_3.py
+++ problem_3.py
@@ -39,45 +39,6 @@ def runToTextInData(data, text, path):
                     return path
 
 
-
-
-# def runToTextInArray(data, text, path):
-
-#     for someValue in data:
-#         path += " -> "
-#         path += str(data.index(someValue))
-
-#         if type(someValue) == dict:
-#             #print("Found Dictionary: " + str(someValue))
-#             runToTextInDictionary(someValue, text, path) #recursion!
-#         if type(someValue) == type([]):
-#             #print("Found Array: " + str(someValue))
-#             runToTextInArray(someValue, text, path)
-#         if type(someValue) == type("string"):
-#             #print("Found String: " + str(someValue))
-#             if str(someValue) == text:
-#                 print("Kind of Victory Array Path: " + path)
-#                 return path
-
-
-# def runToTextInDictionary(data, text, path):
-#     for someKey in data:
-#         #print("Testing " + str(someKey))
-#         #path += str(someKey)
-#         path += " -> "
-#         path += str(someKey)
-#         someValue = data[someKey]
-#         if type(someValue) == dict:
-#             #print("Found Dictionary: " + str(someValue))
-#             runToTextInDictionary(someValue, text, path) #recursion!
-#         if type(someValue) == type([]):
-#             #print("Found Array: " + str(someValue))
-#             runToTextInArray(someValue, text, path)
-#         if type(someValue   ) == type("string"):
-#             #print("Found String: " + str(someValue))
-#             if str(someValue) == text:
-#                 print("Successfully determined path to: " + text + " as: " + path)
-
 globalPath = ""
 
 serializedData = '{"itemList": {"items": [{"id": "item1"},{"id": "item2","label": "Item 2"},{"id": "item3"},{"id": "item4"},{"id": "item5"},{"id": "subItem1","subItems": [{"id": "subItem1Item1","label": "SubItem 1"},{"id": "subItem1Item2","label": "SubItem 2"},{"id": "subItem1Item3","label": "SubItem 3"}]},{"id": "item6"},{"id": "item7","label": "Item 7"},{"id": "subItem2","subItems": {"id": "item1","label": "SubItem 2 item1"}}]}}'
