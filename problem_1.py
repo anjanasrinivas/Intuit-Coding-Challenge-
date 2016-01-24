f3aecdc64f9d72aade09a7549ea7db6e9f4f7397
anjanasrinivas <anjana.srinivas@berkeley.edu>
2016-01-23 17:23:22 -0800

    Added Problem 1 File

---
+++ problem_1.py
@@ -0,0 +1,48 @@
+def find_prime(array): 
+	num_primes = 0
+	max_limit = 10000000
+
+	sieve = []
+	for i in range(max_limit): 
+		sieve[i] = False 
+
+	for i in range(2, max_limit): 
+		if sieve([i] == True: 
+			break 
+		
+		num_primes += 1 
+
+		for j in range (i+i,max_limit,i):
+			sieve[j] = True 
+
+
+
+
+	div = 2 
+
+	while (div != len(lst)-1): 
+		
+		for i in range(len(lst)): 
+			print (i)
+			if i%div == 0: 
+				lst[i]=False 
+		
+			for value in lst: 
+				print (value)
+				if value == True: 
+				
+					div = lst.index(value)
+
+
+	
+	prime = None 
+	count_prime = 0 
+	for i in range(len(lst)): 
+		if count_prime == n: 
+			prime = lst.index(i)
+			break 
+		elif lst[i] == True: 
+			count_prime += 1 
+
+	return prime 
+
