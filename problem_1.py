0911bf3958e2de689c6b757b13a8e7f1469d23a5
anjanasrinivas <anjana.srinivas@berkeley.edu>
2016-01-24 10:42:55 -0800

    sorting list

--- problem_1.py
+++ problem_1.py
@@ -1,8 +1,18 @@
 
-def sort(lst): 
-	lst.sort() 
+
+lst = [1,10,5,63,29,71,10,12,44,29,10,-1]
+
+
 def sorted_list(lst): 
 	for i in range (len(lst)): 
 		for j in range (len(lst)-1-i): 
 			if lst[j] > lst[j+1]: 
 				lst [j], lst[j+1] = lst[j+1], lst[j]
+sorted_list(lst)
+print(lst)
+
+#Using python built in feature to sort
+def sort(lst): 
+	lst.sort() 
+
+
