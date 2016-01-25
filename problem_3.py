a7ce102faa3f21c98cae399ab46ac6722b70ea72
anjanasrinivas <anjana.srinivas@berkeley.edu>
2016-01-24 22:18:58 -0800

    bug fixed

--- problem_3.py
+++ problem_3.py
@@ -1,50 +1,84 @@
 import json 
-import copy
-   
-def runToTextInData(data, text, path):
-    if type(data) == dict :
+
+def runToTextInData(data, text, depth, path):
+    
+    if type(data) == dict:
         for someKey in data:
-        #print("Testing " + str(someKey))
-        #path += str(someKey)
-            path += " -> "
-            path += str(someKey)
-            someValue = data[someKey]
+
             
-            if type(someValue) == dict:
-            #print("Found Dictionary: " + str(someValue))
-                runToTextInData(someValue, text, copy.copy(path)) #recursion!
-            if type(someValue) == type([]):
-            #print("Found Array: " + str(someValue))
-                runToTextInData(someValue, text, copy.copy(path))
-            if type(someValue) == type("string"):
-            #print("Found String: " + str(someValue))
-                if str(someValue) == text:
-                    print("Successfully determined path to: " + text + " as: " + path)
 
+            if len(path) <= depth:
+                path.append(str(someKey))
+            else:
+                path[depth] = str(someKey)
+            
+            someValue = data[someKey]
+
+            if type(someValue) == dict:
+             
+                runToTextInData(someValue, text, depth + 1, path)
+
+            if type(someValue) == type([]):
+                
+                runToTextInData(someValue, text, depth + 1, path)
+
+            if type(someValue) is type('str'):
+               
+                if str(someValue) == text:
+                    output = ''
+                    for element in path: 
+                        if path.index(element) == 0: 
+                            output += element 
+                        if element[:1].startswith('['): 
+                            output += element 
+                        else: 
+                            output += '/' + element
+                    print("Successfully determined path to: " + text + ": " + output)
+                    return
+    
     if type(data) == type([]) : 
+
         for someValue in data:
-            path += " -> "
-            path += str(data.index(someValue))
+            if len(path) <= depth:
+                path.append( "[" + str(data.index(someValue)) + "]")
+            else:
+                
+                path[depth] = ("[" + str(data.index(someValue)) + "]")
 
             if type(someValue) == dict:
-            #print("Found Dictionary: " + str(someValue))
-                runToTextInData(someValue, text, copy.copy(path)) #recursion!
+                
+                runToTextInData(someValue, text, depth + 1, path)
             if type(someValue) == type([]):
-            #print("Found Array: " + str(someValue))
-                runToTextInData(someValue, text, copy.copy(path))
-            if type(someValue) == type("string"):
-            #print("Found String: " + str(someValue))
+                
+                runToTextInData(someValue, text, depth + 1, path)
+            if type(someValue) is type('str'):
+               
                 if str(someValue) == text:
-                    print("Kind of Victory Array Path: " + path)
-                    return path
+                
+                    output = ''
+                    for elements in path: 
+                        if path.index(element) == 0: 
+                            output += element 
+                        if element[:1].startswith('['): 
+                            output += element 
+                        else: 
+                            output += '/' +  element 
 
+                    print("Successfully determined path to: " + text + " as: " + output)
+                    return
 
-globalPath = ""
+globalPath = []
 
 serializedData = '{"itemList": {"items": [{"id": "item1"},{"id": "item2","label": "Item 2"},{"id": "item3"},{"id": "item4"},{"id": "item5"},{"id": "subItem1","subItems": [{"id": "subItem1Item1","label": "SubItem 1"},{"id": "subItem1Item2","label": "SubItem 2"},{"id": "subItem1Item3","label": "SubItem 3"}]},{"id": "item6"},{"id": "item7","label": "Item 7"},{"id": "subItem2","subItems": {"id": "item1","label": "SubItem 2 item1"}}]}}'
 
 deserializedData = json.loads(serializedData)
-print(runToTextInData(deserializedData, "item2", globalPath))
+runToTextInData(deserializedData, "item2", 0, globalPath)
+runToTextInData(deserializedData, "subItem1", 0, globalPath)
+
+
+
+
+
 
 
 
